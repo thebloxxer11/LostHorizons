2f3e9ceceb1be94f5c27afc3a474a05a79e90d07
thebloxxer11 <legokiddotoo@gmail.com>
2023-10-12 20:42:29 -0700

    Restart Progress

--- src/main/java/net/tb11/LostHorizons/worldgen/feature/CrystalSpikeFeature.java
+++ src/main/java/net/tb11/LostHorizons/worldgen/feature/CrystalSpikeFeature.java
@@ -2,6 +2,7 @@ package net.tb11.LostHorizons.worldgen.feature;
 
 import com.mojang.serialization.Codec;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
@@ -9,7 +10,7 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
 import net.tb11.LostHorizons.LostHorizons;
 
-
+//USE THE EQUATION HERE: https://www.geogebra.org/calculator/fwdvgmty
 public class CrystalSpikeFeature extends Feature<CrystalSpikeFeatureConfig>{
     public CrystalSpikeFeature(Codec<CrystalSpikeFeatureConfig> configCodec){
         super(configCodec);
@@ -20,5 +21,10 @@ public class CrystalSpikeFeature extends Feature<CrystalSpikeFeatureConfig>{
         CrystalSpikeFeatureConfig config = context.getConfig();
         
         return true;
+    }
+    private void generateFeature(Integer r, Integer h, BlockPos castPos, BlockState block) {
+        Integer castX = castPos.getX();
+        Integer castY = castPos.getY();
+        Integer castZ = castPos.getZ();
     }
 }
