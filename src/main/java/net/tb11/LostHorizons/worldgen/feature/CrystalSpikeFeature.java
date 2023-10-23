b7db7bd41957115c35efc9100b98f52aa0a90e8e
thebloxxer11 <legokiddotoo@gmail.com>
2023-10-23 08:20:02 -0700

    WHY 2

--- src/main/java/net/tb11/LostHorizons/worldgen/feature/CrystalSpikeFeature.java
+++ src/main/java/net/tb11/LostHorizons/worldgen/feature/CrystalSpikeFeature.java
@@ -1,30 +1,30 @@
-package net.tb11.LostHorizons.worldgen.feature;
+// package net.tb11.LostHorizons.worldgen.feature;
 
-import com.mojang.serialization.Codec;
+// import com.mojang.serialization.Codec;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.util.FeatureContext;
-import net.tb11.LostHorizons.LostHorizons;
+// import net.minecraft.block.BlockState;
+// import net.minecraft.block.Blocks;
+// import net.minecraft.util.math.BlockPos;
+// import net.minecraft.util.math.ChunkPos;
+// import net.minecraft.world.gen.feature.Feature;
+// import net.minecraft.world.gen.feature.util.FeatureContext;
+// import net.tb11.LostHorizons.LostHorizons;
 
-//USE THE EQUATION HERE: https://www.geogebra.org/calculator/fwdvgmty
-public class CrystalSpikeFeature extends Feature<CrystalSpikeFeatureConfig>{
-    public CrystalSpikeFeature(Codec<CrystalSpikeFeatureConfig> configCodec){
-        super(configCodec);
-    }
+// //USE THE EQUATION HERE: https://www.geogebra.org/calculator/fwdvgmty
+// public class CrystalSpikeFeature extends Feature<CrystalSpikeFeatureConfig>{
+//     public CrystalSpikeFeature(Codec<CrystalSpikeFeatureConfig> configCodec){
+//         super(configCodec);
+//     }
 
-    @Override
-    public boolean generate(FeatureContext<CrystalSpikeFeatureConfig> context){
-        CrystalSpikeFeatureConfig config = context.getConfig();
+//     @Override
+//     public boolean generate(FeatureContext<CrystalSpikeFeatureConfig> context){
+//         CrystalSpikeFeatureConfig config = context.getConfig();
         
-        return true;
-    }
-    private void generateFeature(Integer r, Integer h, BlockPos castPos, BlockState block) {
-        Integer castX = castPos.getX();
-        Integer castY = castPos.getY();
-        Integer castZ = castPos.getZ();
-    }
-}
+//         return true;
+//     }
+//     private void generateFeature(Integer r, Integer h, BlockPos castPos, BlockState block) {
+//         Integer castX = castPos.getX();
+//         Integer castY = castPos.getY();
+//         Integer castZ = castPos.getZ();
+//     }
+// }
