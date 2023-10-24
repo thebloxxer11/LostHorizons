0f2e71c64e7315d7e4fafaaca974a9e51f2a9be3
thebloxxer11 <legokiddotoo@gmail.com>
2023-10-24 07:01:26 -0700

    Hopefully fix Compilation

--- src/main/java/net/tb11/LostHorizons/LostHorizonsClient.java
+++ src/main/java/net/tb11/LostHorizons/LostHorizonsClient.java
@@ -1,9 +1,11 @@
 package net.tb11.LostHorizons;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
-
+@Environment(EnvType.CLIENT)
 public class LostHorizonsClient implements ClientModInitializer {
 
     @Override
