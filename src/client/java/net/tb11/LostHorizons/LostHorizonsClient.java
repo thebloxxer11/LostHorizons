59748e1b463d3292094151a9dc2c9a132d778e16
jkaiser <legokiddotoo@gmail.com>
2024-01-24 14:15:55 -0800

    Update file locations, implement Floating Crystal to TralyxConductorBlockEntityModel

--- src/client/java/net/tb11/LostHorizons/LostHorizonsClient.java
+++ src/client/java/net/tb11/LostHorizons/LostHorizonsClient.java
@@ -4,6 +4,7 @@
  import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
  import net.minecraft.client.render.RenderLayer;
  import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+ import net.tb11.LostHorizons.block_entity_models.TralyxConductorBlockEntityModel;
 
  public class LostHorizonsClient implements ClientModInitializer {
 
--- src/client/java/net/tb11/LostHorizons/block_entity_models/TralyxConductorBlockEntityModel.java
+++ src/client/java/net/tb11/LostHorizons/block_entity_models/TralyxConductorBlockEntityModel.java
@@ -1,17 +1,25 @@
-package net.tb11.LostHorizons;
+package net.tb11.LostHorizons.block_entity_models;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.math.RotationAxis;
 import net.tb11.LostHorizons.tralyx_energy.block_entities.TralyxConductorBlockEntity;
 
 public class TralyxConductorBlockEntityModel implements BlockEntityRenderer<TralyxConductorBlockEntity> {
+    private static ItemStack crystal = new ItemStack(Items.AMETHYST_SHARD, 1);
     public TralyxConductorBlockEntityModel(BlockEntityRendererFactory.Context ctx){
-
     }
     @Override
     public void render(TralyxConductorBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         matrices.push();
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees((entity.getWorld().getTime() + tickDelta)*4.0f));
+        MinecraftClient.getInstance().getItemRenderer().renderItem(crystal, ModelTransformationMode.GROUND, light, overlay, matrices, vertexConsumers, entity.getWorld(), 0);
+        matrices.pop();
     }
 }
