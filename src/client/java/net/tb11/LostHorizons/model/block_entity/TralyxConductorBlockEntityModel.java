d11bd06a73e38f82d7ae5ffba64a66a695a8ecfa
jkaiser <legokiddotoo@gmail.com>
2024-01-27 18:48:56 -0800

    Fix Rendering of Tralyx Conductor Base

--- src/client/java/net/tb11/LostHorizons/model/block_entity/TralyxConductorBlockEntityModel.java
+++ src/client/java/net/tb11/LostHorizons/model/block_entity/TralyxConductorBlockEntityModel.java
@@ -15,8 +15,10 @@ import net.tb11.LostHorizons.tralyx_energy.block_entities.TralyxConductorBlockEn
 import java.util.Objects;
 
 public class TralyxConductorBlockEntityModel implements BlockEntityRenderer<TralyxConductorBlockEntity> {
-    private static final ItemStack crystal = new ItemStack(Items.AMETHYST_SHARD, 1);
-    public TralyxConductorBlockEntityModel(BlockEntityRendererFactory.Context ctx){}
+    private static final ItemStack CRYSTAL = new ItemStack(Items.AMETHYST_SHARD, 1);
+    public TralyxConductorBlockEntityModel(BlockEntityRendererFactory.Context ctx){
+
+    }
     @Override
     public void render(TralyxConductorBlockEntity entity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
         matrices.push();
@@ -25,7 +27,7 @@ public class TralyxConductorBlockEntityModel implements BlockEntityRenderer<Tral
         matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(45.0F));
         matrices.translate(0.125F,0.0F,0.0F);
         int lightAbove = WorldRenderer.getLightmapCoordinates(Objects.requireNonNull(entity.getWorld()), entity.getPos().up());
-        MinecraftClient.getInstance().getItemRenderer().renderItem(crystal, ModelTransformationMode.GROUND, lightAbove, overlay, matrices, vertexConsumers, entity.getWorld(), 0);
+        MinecraftClient.getInstance().getItemRenderer().renderItem(CRYSTAL, ModelTransformationMode.GROUND, lightAbove, overlay, matrices, vertexConsumers, entity.getWorld(), 0);
         matrices.pop();
     }
 }
--- src/main/java/net/tb11/LostHorizons/tralyx_energy/blocks/TralyxConductor.java
+++ src/main/java/net/tb11/LostHorizons/tralyx_energy/blocks/TralyxConductor.java
@@ -1,13 +1,13 @@
 package net.tb11.LostHorizons.tralyx_energy.blocks;
 
 import com.mojang.serialization.MapCodec;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.tb11.LostHorizons.LostHorizons;
 import net.tb11.LostHorizons.tralyx_energy.block_entities.TralyxConductorBlockEntity;
@@ -33,4 +33,7 @@ public class TralyxConductor extends BlockWithEntity{
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
         return validateTicker(type, LostHorizons.TRALYX_CONDUCTOR_BLOCK_ENTITY, (world1, pos, state1, be) -> TralyxConductorBlockEntity.tick(world1, pos, state1, be));
     }
+//    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context){
+//        return Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+//    }
 }
