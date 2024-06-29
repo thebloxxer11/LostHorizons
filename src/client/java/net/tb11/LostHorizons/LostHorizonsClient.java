543103af4c84dd2d3fda79b614d6c1c97643ecde
jkaiser <legokiddotoo@gmail.com>
2024-06-28 18:36:40 -0700

    (1.21) Fix Tralyx Conductor Renderer, Remove Deprecated Library

--- src/client/java/net/tb11/LostHorizons/LostHorizonsClient.java
+++ src/client/java/net/tb11/LostHorizons/LostHorizonsClient.java
@@ -2,11 +2,14 @@ package net.tb11.LostHorizons;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+import net.tb11.LostHorizons.model.block_entity.TralyxConductorBlockEntityModel;
+import net.tb11.LostHorizons.tralyx_energy.block_entities.TralyxConductorBlockEntity;
 
 
 public class LostHorizonsClient implements ClientModInitializer {
-    // TODO: This code is giving me hell, compile failure
     @Override
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), LostHorizons.Arcanite_Crystal);
@@ -15,6 +18,8 @@ public class LostHorizonsClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), LostHorizons.Crylatite_Crystal);
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), LostHorizons.Noxite_Crystal);
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), LostHorizons.Shoctite_Crystal);
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), LostHorizons.Tralyx_Conductor);
+        BlockEntityRendererFactories.register(LostHorizons.TRALYX_CONDUCTOR_BLOCK_ENTITY, TralyxConductorBlockEntityModel::new);
     }
     
 }
